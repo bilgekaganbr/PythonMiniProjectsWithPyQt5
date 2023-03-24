620626eddde647af0a4b68885ed7bfbb4149818d
Bilge Kağan Börekçi <bilgekaganbr@gmail.com>
2023-03-24 13:14:09 +0300

    Add files via upload

---
+++ clear_or_print.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5 import QtWidgets
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        #the function that will be called by init function
+        self.init_ui()
+
+    def init_ui(self):
+
+        #define a label
+        self.text_field = QtWidgets.QLineEdit()
+        #define a 'clear' button
+        self.clear = QtWidgets.QPushButton("Clear")
+        #define a 'print' button
+        self.print = QtWidgets.QPushButton("Print")
+
+        #define a vertical box layout
+        v_box = QtWidgets.QVBoxLayout()
+
+        #add the label and two buttons to the vertical box layout then add a stretch so that all the components stay at top
+        v_box.addWidget(self.text_field)
+        v_box.addWidget(self.clear)
+        v_box.addWidget(self.print)
+        v_box.addStretch()
+
+        #set the vertical box layout to the window object
+        self.setLayout(v_box)
+
+        #connect both buttons to the function
+        self.clear.clicked.connect(self.click)
+        self.print.clicked.connect(self.click)
+
+        self.show()
+
+    def click(self):
+
+        #define a sender so that object can understand which button is clicked
+        sender = self.sender()
+
+        if sender.text() == "Clear":
+
+            #if the clear button is clicked then erase the text in the label
+            self.text_field.clear()
+
+        else:
+
+            #else print the text in the label
+            print(self.text_field.text())
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = Window()
+
+sys.exit(app.exec_())
---
+++ click_and_count.py
@@ -0,0 +1,59 @@
+import sys
+from PyQt5 import QtWidgets
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        #the function that will be called by init function
+        self.init_ui()
+
+    def init_ui(self):
+
+        #define a label
+        self.text_field = QtWidgets.QLabel("Not clicked on me yet...")
+        #define a button
+        self.button = QtWidgets.QPushButton("Click me...")
+        #initialize the count from 0
+        self.count = 0
+
+        #define a vertical box layout
+        v_box = QtWidgets.QVBoxLayout()
+
+        #add the button and the label to the vertical box layout and then add stretch so that all the components stay
+        #at top
+        v_box.addWidget(self.button)
+        v_box.addWidget(self.text_field)
+        v_box.addStretch()
+
+        #define a horizontal box layout
+        h_box = QtWidgets.QHBoxLayout()
+
+        #add a stretch to the left and the right side then add the vertical box layout to the middle
+        h_box.addStretch()
+        h_box.addLayout(v_box)
+        h_box.addStretch()
+
+        #set the horizontal box layout to the window object
+        self.setLayout(h_box)
+
+        #connect the button to the function
+        self.button.clicked.connect(self.click)
+
+        self.show()
+
+    def click(self):
+
+        #when the button is clicked, increase the count by 1
+        self.count += 1
+        #update the text in the label
+        self.text_field.setText("Clicked on me " + str(self.count) + " times...")
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = Window()
+
+sys.exit(app.exec_())
+
